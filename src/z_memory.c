94a29202a3f53a78dfa84d74f6c353cf6371e538
meauxdal <68095521+meauxdal@users.noreply.github.com>
2026-02-23 00:20:11 -0500

    b20

--- src/z_memory.c
+++ src/z_memory.c
@@ -1,39 +1,25 @@
 #include <stdint.h>
 #include <gb/gb.h>
 #include "z_memory.h"
-#include "zork_data.h"
+#include "zork_data/zork_data.h"
 
-/* Helper: fetch byte from banked ROM */
 uint8_t z_read_byte(uint32_t address) {
     uint8_t value;
     uint8_t save_bank = _current_bank;
 
-    /* Determine bank number and offset */
-    uint8_t bank = ZORK_DATA_BANK_START + (address / ZORK_BANK_SIZE);
-    uint16_t offset = address % ZORK_BANK_SIZE;
+    uint8_t bank = (uint8_t)(address >> 14) + ZORK_DATA_BANK_START;
+    uint16_t offset = (uint16_t)(address & 0x3FFF);
 
-    /* Switch to the correct bank */
     SWITCH_ROM(bank);
-
-    /* Use pointer arithmetic on banked array */
     switch (bank) {
-#define BANK_CASE(n) case n: value = zork_bank##n##_data[offset]; break;
-#if ZORK_DATA_NUM_BANKS >= 1
-        BANK_CASE(7)
-#endif
-#if ZORK_DATA_NUM_BANKS >= 2
-            BANK_CASE(8)
-#endif
-#if ZORK_DATA_NUM_BANKS >= 3
-            BANK_CASE(9)
-#endif
-#if ZORK_DATA_NUM_BANKS >= 4
-            BANK_CASE(10)
-#endif
+    case 1: value = zork_bank1_data[offset]; break;
+    case 2: value = zork_bank2_data[offset]; break;
+    case 3: value = zork_bank3_data[offset]; break;
+    case 4: value = zork_bank4_data[offset]; break;
+    case 5: value = zork_bank5_data[offset]; break;
+    case 6: value = zork_bank6_data[offset]; break;
     default: value = 0; break;
     }
-
-    /* Restore previous bank */
     SWITCH_ROM(save_bank);
     return value;
 }
@@ -42,10 +28,13 @@ uint16_t z_read_word(uint32_t address) {
     return ((uint16_t)z_read_byte(address) << 8) | z_read_byte(address + 1);
 }
 
-/* Writes are no-ops for ROM story data */
-void z_write_byte(uint32_t address, uint8_t value) { (void)address; (void)value; }
-void z_write_word(uint32_t address, uint16_t value) { (void)address; (void)value; }
+void z_write_byte(uint32_t address, uint8_t value) {
+    (void)address; (void)value;
+}
+
+void z_write_word(uint32_t address, uint16_t value) {
+    (void)address; (void)value;
+}
 
 void z_init_memory(void) {
-    /* Nothing to do for ROM-only story data */
 }
